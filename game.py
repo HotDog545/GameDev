b711694556f8c2e0f160906ad36aab38c621ba21
pete <pete@stridebird.com>
2016-09-25 21:51:29 +0100

    version 2
    note farm_map_2.png does not exist...!

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import math
+
 pygame.init()
 
 title = "KFPoop"
@@ -10,47 +11,101 @@ display_height= 700
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption(title)
 clock = pygame.time.Clock()
-
+icon = pygame.image.load("img/icon.ico")
+pygame.display.set_icon(icon)
 bg = pygame.image.load("img/farm_map.png").convert()
+bg2 = pygame.image.load("img/farm_map_2.png").convert()
 
 black = (0,0,0)
 white = (0,0,255)
-x = 0
-y = 475
-animalImg = pygame.image.load("img/chicken.png")
+
+animalchar = pygame.image.load("img/chicken.png")
+
+animal_width = 75
 
 
-        
-animal_speed = 0
-
-gameStart = True
-
-while gameStart == True:
+#def collision():
     
-    for event in pygame.event.get():
+
+def score():
+    score = 0
+
+def animal(x,y):
+    gameDisplay.blit(animalchar, [x,y])
+
+def text_objects(text, font):
+     textSurface = font.render(text, True, black)
+     return textSurface, textSurface.get_rect()
+    
+def message_display(text):
+     largeText = pygame.font.Font('freesansbold.ttf',115)
+     TextSurf, TextRect = text_objects(text, largeText)
+     TextRect.center = ((display_width/2),(display_height/2))
+     gameDisplay.blit(TextSurf, TextRect)
+     pygame.display.update()
+     time.sleep(2)
+     game_loop()
+
+def die():
+     message_display('You Died')     
+
+#class Enemy(object):
+#    def move_towards_player(self, player):
+#        dx, dy = self.rect.x - player.rect.x, self.rect.y - player.rect.y
+#        dist = math.hypot(dx, dy)
+#        dx, dy = dx / dist, dy / dist
+#        self.rect.x += dx * self.speed
+#        self.rect.y += dy * self.speed
+
+def gameloop():
+    x = 0
+    y = 475
+    
+   
+    
+    x_change = 0
+
+    gameStart = True
+
+    while gameStart:
+    
+        for event in pygame.event.get():
         
-        if event.type==pygame.QUIT:
-            gameStart=False
+            if event.type==pygame.QUIT:
+                gameStart=True
 
-        if event.type==pygame.KEYDOWN:
-            if event.key==pygame.K_UP:
-                y-=25
-            if event.key==pygame.K_DOWN:
-                y+=25
+            if event.type==pygame.KEYDOWN:
+                if event.key==pygame.K_UP:
+                    y-=25
 
-            if event.key==pygame.K_LEFT:
-                x-=25
+                if event.key==pygame.K_DOWN:
+                    y+=25
 
-            if event.key==pygame.K_RIGHT:
-                x+=25
+                if event.key==pygame.K_LEFT:
+                    x-=25
 
-    gameDisplay.blit(bg, [0,0])
-    animal(x,y)
-       
-    pygame.display.update()
-    clock.tick(30)
+                if event.key==pygame.K_RIGHT:
+                    x+=25
 
 
 
+        x += x_change
+
+
+        gameDisplay.blit(bg, [0,0])
+        animal(x,y)
+
+        
+        if x < 0:
+            gameDisplay.blit(bg2, [0,0])
+            animal(x,y)
+
+        
+
+        pygame.display.update()
+        clock.tick(60)
+
+
+gameloop()
 pygame.quit()
 quit()
